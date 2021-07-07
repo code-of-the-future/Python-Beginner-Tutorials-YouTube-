7f9ce6a9c35a886bc621e6a1ca3552a75ec0e2f6
Code of the Future <75938783+code-of-the-future@users.noreply.github.com>
2021-07-07 09:29:24 +0100

    Update Password_With_Special_Characters.py

--- Python Projects/Password_With_Special_Characters.py
+++ Python Projects/Password_With_Special_Characters.py
@@ -8,9 +8,10 @@ special_chars = list(punctuation)  # List has 32 elements in (can determine this
 
 # Set up the password generator as before
 password = ""  # Create an empty string
-for i in range(5): # We want our password to 15 characters (3*5 = 15)
+for a in range(5):  # We want our password to 15 characters (3*5 = 15)
     i = chr(random.randint(65, 90))  # Creates a random upper case character
     j = chr(random.randint(65, 90)).lower()  # Creates a random lower case character
     k = special_chars[random.randint(0, 31)]  # Creates a random special character from our list 'special_chars'
-    password = str(password) + i + j + k  # Takes our empty string and adds the random characters we have assigned above. This is repeated 5 times.
+    password = str(password) + i + j + k  # Takes our empty string and adds the random characters we have assigned
+    # above. This is repeated 5 times.
 print(password)
